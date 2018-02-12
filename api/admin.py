1e507c1a66f4c3454789a4b775129c07abd79d22
Julien Capellari <julien@capellari.net>
2018-02-12 16:01:30 +0100

    Modèle Horaire

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
+# Importations
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    # Liste
+    list_display = ("nom", "pluriel")
+    search_fields = ("nom",)
--- api/models.py
+++ api/models.py
@@ -2,9 +2,27 @@
 from django.db import models
 
 
-# Models
+# Modèles
 class Type(models.Model):
     # Attributs
     nom = models.CharField(max_length=100)
     pluriel = models.CharField(max_length=100)
     blacklist = models.BooleanField(default=False, null=False, blank=True)
+
+
+class Horaire(models.Model):
+    #  Jours
+    JOURS = (
+        (0, "dimanche"),
+        (1, "lundi"),
+        (2, "mardi"),
+        (3, "mercredi"),
+        (4, "jeudi"),
+        (5, "vendredi"),
+        (6, "samedi"),
+    )
+
+    # Champs
+    jour = models.SmallIntegerField(choices=JOURS)
+    open = models.SmallIntegerField(verbose_name="ouverture")
+    close = models.SmallIntegerField(verbose_name="fermeture", default=0)
