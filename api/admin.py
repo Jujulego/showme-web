ee7d526a32859afa260d6dcebc2e970cc46d970d
Julien CAPELLARI <julien@capellari.net>
2018-02-12 16:57:59 +0100

    Modèle Lieu

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 # Importations
 from django.contrib import admin
+from django.db.models import Q
 
 from .models import *
 
@@ -9,4 +10,33 @@ from .models import *
 class TypeAdmin(admin.ModelAdmin):
     # Liste
     list_display = ("nom", "pluriel")
+    search_fields = ("nom",)
+
+
+@admin.register(Lieu)
+class LieuAdmin(admin.ModelAdmin):
+    # Filtres
+    class PhotoFiltre(admin.SimpleListFilter):
+        # Attributs
+        title = "photo"
+        parameter_name = "photo"
+
+        # Méthodes
+        def lookups(self, request, model_admin):
+            return [
+                ("v", "avec"),
+                ("f", "sans"),
+            ]
+
+        def queryset(self, request, queryset):
+            if self.value() == "v":
+                return queryset.filter(~Q(photo=""))
+            elif self.value() == "f":
+                return queryset.filter(photo="")
+
+            return queryset
+
+    # Liste
+    list_display = ("nom", "latitude", "longitude", "a_photo")
+    list_filter = (PhotoFiltre,)
     search_fields = ("nom",)
--- api/models.py
+++ api/models.py
@@ -1,17 +1,71 @@
 # Importations
-from django.db import models
+from django.contrib.gis.db import models
 
 
 # Modèles
-class Type(models.Model):
+class Lieu(models.Model):
     # Attributs
+    PRIX = (
+        (None, "----------"),
+        (0, "Gratuit"),
+        (1, "Bon marché"),
+        (2, "Modéré"),
+        (3, "Cher"),
+        (4, "Très cher"),
+    )
+
+    # Champs obligatoires
+    position = models.PointField(spatial_index=True)
+    nom = models.CharField(max_length=255, db_index=True, default="Inconnu", blank=True)
+
+    # Champs additionnels
+    telephone = models.CharField(max_length=20, default="", blank=True, verbose_name="téléphone")
+    note = models.FloatField(null=True, blank=True)
+    site = models.URLField(max_length=500, null=True, blank=True)
+    prix = models.SmallIntegerField(default=None, blank=True, choices=PRIX)
+    photo = models.ImageField(upload_to="photos/", max_length=500, null=True, blank=True)
+
+    # Méta
+    class Meta:
+        verbose_name_plural = "lieux"
+
+    # Méthodes
+    def a_photo(self):
+        return self.photo.name
+
+    a_photo.short_description = "photo"
+    a_photo.admin_order_field = "photo"
+    a_photo.boolean = True
+
+    # Propriétés
+    # - latitude
+    @property
+    def latitude(self):
+        return self.position.y
+
+    @latitude.setter
+    def latitude(self, lat):
+        self.position.y = lat
+
+    # - longitude
+    @property
+    def longitude(self):
+        return self.position.x
+
+    @longitude.setter
+    def longitude(self, lng):
+        self.position.x = lng
+
+
+class Type(models.Model):
+    # Champs
     nom = models.CharField(max_length=100)
     pluriel = models.CharField(max_length=100)
     blacklist = models.BooleanField(default=False, null=False, blank=True)
 
 
 class Horaire(models.Model):
-    #  Jours
+    # Attributs
     JOURS = (
         (0, "dimanche"),
         (1, "lundi"),
